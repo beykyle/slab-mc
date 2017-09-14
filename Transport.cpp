e2b00ee6dcc43fc813da6e463e91a3ad2ac4906d
Kyle Beyer <beykyle@umich.edu>
2017-09-14 10:36:55 -0400

    Changed interface of transport()

--- Transport.cpp
+++ Transport.cpp
@@ -9,8 +9,16 @@
 #include <numeric>
 
 #include "Random.h"
+#include "boost/variant.hpp"
 
 //TODO make input class  in Input.cpp and Input.h, with read from file function
+
+class Particle {
+  public:
+    double x, mu;
+    int    cell;
+    bool   alive;
+};
 
 class Slab {
   public:
@@ -21,23 +29,64 @@ class Slab {
     std::vector <double> fission_xs;
 };
 
-class Input {
-  public:  
-    Slab    slab;
-    int     tallyBins;
-    double  nHistories;
-};
 
 class Leakage {
   public:
-    double leakage_squared;
-    double leakahe_hist;
-    //function to implement leakage counting
+    bool     vector;
+    bool     total;
+    int      nbins;
+
+    void     incrementCount(void);
+    void     setVal(double nHistories , Slab s);
+  private:
+    double   count;
+    double   squared;
+    double   hist;
+    double   value;
+    double   uncertainty;
+};
+
+void Leakage::setVal(double nHistories , Slab s) { 
+  // Finalizes the leakage tally and calculates uncertainty
+  value =  count / nHistories;
+  uncertainty = sqrt( ( squared / nHistories - pow(value,2) ) / nHistories );
+};
+
+void Leakage::incrementCount(void) {
+  count += 1.0;
 };
 
 class Flux {
   public:
-    //functions to implement flux_tally
+    bool                 vector;
+    bool                 total;
+    int                  nbins;
+
+    void                 setVal(double nhistories , Slab s);
+    void                 addLength(int i , double dist);
+  private:
+    double               value;
+    double               uncertainty;
+    std::vector <double> path_lengths;
+    std::vector <double> values;
+    std::vector <double> uncertainties;
+};
+
+void Flux::addLength(int i , double dist) {
+  path_lengths[i] += dist;
+};
+
+void Flux::setVal(double nHistories , Slab s) {  
+  // Finalizes the flux tally and calculates uncertainty
+  double  sum_path_lengths = 0;
+
+  for(int i = 0; i < path_lengths.size(); ++i) {
+    values.push_back(path_lengths[i] / nHistories / (s.width / nbins ) );
+    uncertainties.push_back(0.0); 
+  }
+
+  value = sum_path_lengths;
+  uncertainty = 0.0; 
 };
 
 class Tally {
@@ -46,23 +95,23 @@ class Tally {
     bool                 total;
     double               value , uncertainty;
     std::vector <double> values, uncertainties;
-    int nbins;
 };
 
-
-class Particle {
-  public:
-    double x, mu;
-    int    cell;
-    bool   alive;
+class Input {
+  public:  
+    Slab                          slab;
+    int                           tallyBins;
+    double                        nHistories;
+    std::map<std::string , boost::variant <Leakage , Flux > > tallies;
 };
+
 
 // ********************************************************************************************************* //
 //  Output functions: take in a dictionary of Tally objects, and the duration of time required to run        //
 //      transport(). Outputs results to terminal and an output file "tallies.out"                            //
 // ********************************************************************************************************* //
 
-void terminal_out(std::map<std::string , Tally> tallies , double duration) {
+void terminal_out(std::map<std::string , boost::variant <Leakage , Flux > > tallies , double duration) {
 // a function to output the results of the simulation to the terminal
 // requires: a dictionary of Tally objects
 
@@ -88,7 +137,7 @@ void terminal_out(std::map<std::string , Tally> tallies , double duration) {
   
 };
 
-void file_out(Slab slab , std::map<std::string , Tally> tallies , double duration) {
+void file_out(Slab slab , std::map<std::string , boost::variant <Leakage , Flux > > tallies , double duration) {
 // a function to output the vector tallies  of the simulation to a file
 // requires: a dictionary of Tally objects
   
@@ -124,100 +173,9 @@ void file_out(Slab slab , std::map<std::string , Tally> tallies , double duratio
 //	                   							         	             //
 // ********************************************************************************************************* //
 
-void transport(Tally &leak_tally , Tally &flux_tally, Input I) {
-// this function takes in an input object, runs transport, and outputs a dictionary of tally objects 
+void transport(Tally &leak_tally , Tally &flux_tally, Slab s) {
+// this function takes in a slab object, a number of histories, runs transport, and outputs a dictionary of tally objects 
   
-  //// initialize counters for tally objects ////
-  // leakage
-  double leakage_squared  = 0.0;
-  leak_tally.value        = 0.0;
-  leak_tally.uncertainty  = 0.0;
-  // flux
-  double               sum_path_lengths = 0;
-  std::vector <double> path_lengths(flux_tally.nbins , 0.0);
-  std::vector <double> path_lengths_squared(flux_tally.nbins , 0.0);
-
-  // set flags on tally objects for output formating 
-  leak_tally.vector = false;
-  leak_tally.total  = false;
-  flux_tally.vector = true;
-  flux_tally.total  = true;
-  
-  //// initialaize variables from input object ////
-  int    nHistories      = I.nHistories;
-  
-  double thickness       = I.slab.width;
-  double cellWidth       = thickness / I.slab.nCells;       // thickness of bins for xsec sampling
-  double tWidth          = thickness / I.tallyBins;  // tickness of mesh bins for tallies
-
-  std::vector <double> scatter_ratio(I.slab.nCells , 0.0);
-  for ( int i = 0; i < I.slab.nCells; ++i) {
-    scatter_ratio[i] = I.slab.scatter_xs[i] / I.slab.total_xs[i];
-  }
- 
-  //// loop over histories ////
-  for ( int i = 0 ; i < I.nHistories ; i++ ) { 
-    // generate source particle
-    Particle p;
-    p.x     = 0.0;
-    p.mu    = 1 - Urand(); //sample an isotropic incident particle in 2pi
-    p.cell  = 0;
-    p.alive = true;
-    
-    while ( p.alive ) {
-      // do transport for a single particle
-      
-      // sample new distance to collision
-      double dist_to_collision = -log( Urand() ) / I.slab.total_xs[p.cell];
-      // update particle location and cell
-      p.x += dist_to_collision * p.mu;
-      p.cell = floor(p.x / cellWidth);
-
-      // check for leakage
-      if ( p.x < 0.0 || p.x > thickness ) {
-        // leaked out
-        p.alive = false;
-        if ( p.x > thickness ) {
-          //update leakage tally
-          leak_tally.value += 1.0;
-        }
-      }
-      
-      // determine next transport step
-      if (p.alive == true) {
-        // have a collision
-        if ( Urand() < scatter_ratio[p.cell] ) {
-          // particle scatters
-          p.mu = 2.0 * Urand() - 1.0;
-        }
-        else {
-          // absorbed :(
-          p.alive = false;
-        }
-      }
-    } // end of particle loop
-    
-    //  update tallies
-    leakage_squared += pow(leak_tally.value,2);
-
-  } // we are almost done!
-  
-  //// calculate tallies ////
-  
-  // leakage
-  leak_tally.value =  leak_tally.value / nHistories;
-  leak_tally.uncertainty = sqrt( ( leakage_squared / nHistories - pow(leak_tally.value,2) ) / nHistories );
-  
-  // flux
-  for(int i = 0; i < path_lengths.size(); ++i) {
-    path_lengths_squared[i] = path_lengths[i] * path_lengths[i];
-    sum_path_lengths += path_lengths[i];
-    flux_tally.values.push_back(path_lengths[i] / tWidth / nHistories);
-    flux_tally.uncertainties.push_back(0); 
-  }
-
-  flux_tally.value = sum_path_lengths;
-  flux_tally.uncertainty = 0; 
 };
 
 // ********************************************************************************************************* //
@@ -228,37 +186,41 @@ void transport(Tally &leak_tally , Tally &flux_tally, Input I) {
 int main()  {
   
   // set up transport problem
+  //TODO create input class constructor that parses from input file
   Input I; 
   I.nHistories       = 1e7;
-  I.slab.total_xs    = {1.0 , 1.0 , 1.0, 1.0 , 1.0};
-  I.slab.scatter_xs  = {0.0 , 0.0 , 0.8 , 0.8 , 0.5};
+  I.slab.width       = 4.0;
+  I.slab.nCells      = 4;
+  I.slab.total_xs    = {0.0 , 0.3 , 1.0 , 1.0 } ;
+  I.slab.scatter_xs  = {0.0 , 0.25 , 0.7 , 0.1};
   I.slab.fission_xs  = {0.0};
-  I.slab.width       = 10.0;
-  I.slab.nCells      = 5;
-  
-  // Initialize tally objects 
-  Tally leak_tally;
-  leak_tally.nbins = 1;
-  
-  Tally flux_tally;
-  flux_tally.nbins = 1000;
+
+  // create and populate a dictionary of Tally objects
+  // with the key being the quantity being tallied
+  Leakage leak_tally;
+  Flux    flux_tally;
+  flux_tally.nbins = 1000; //the mesh for the flux tally has 1000 bins
+
+  // set flags for output formating 
+  leak_tally.vector = false;
+  leak_tally.total  = false;
+  flux_tally.vector = true;
+  flux_tally.total  = true;
+
+  I.tallies["Leakage Probability"] = leak_tally;
+  I.tallies["Flux"] = flux_tally;
   
   // run and time the transport function
   std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
-  transport(leak_tally, flux_tally, I);
+  transport(I.tallies["Leakage Probability"], I.tallies["Flux"], I.slab);
   std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
 
   double duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
 
-  // create and populate a dictionary of Tally objects
-  // with the key being the quantity being tallied
-  std::map<std::string , Tally> tallies;
-  tallies["Leakage Probability"] = leak_tally;
-  tallies["Flux"] = flux_tally;
   
   // output results 
-  terminal_out(tallies , duration);
-  file_out(I.slab , tallies , duration); 
+  terminal_out( I.tallies , duration);
+  file_out(I.slab , I.tallies , duration); 
   
   return(0);
 }
