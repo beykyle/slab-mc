9b312e8985e9704dc2cd498d61e90fba607e7073
Kyle Beyer <beykyle@umich.edu>
2017-09-28 05:13:29 -0400

    fixed transport()

--- Transport.cpp
+++ Transport.cpp
@@ -26,7 +26,7 @@ class Particle {
     int    cell;
     bool   alive;
     Particle();
-    Particle(float x , float mu);
+    Particle(double x , double mu);
     ~Particle(void);
 };
 
@@ -37,7 +37,7 @@ Particle::Particle()
   alive = true;
 };
 
-Particle::Particle(float x_in , float mu_in) 
+Particle::Particle(double x_in , double mu_in) 
 {
   x = x_in;
   mu = mu_in;
@@ -48,9 +48,12 @@ class Slab {
   public:
     double               width;
     int                  nCells;
-    std::vector <double> scatter_xs;
-    std::vector <double> total_xs;
-    std::vector <double> fission_xs;
+    double scatter_xs;
+    double total_xs;
+    double fission_xs;
+//    std::vector <double> scatter_xs;
+//    std::vector <double> total_xs;
+//    std::vector <double> fission_xs;
 };
 
 
@@ -144,10 +147,10 @@ std::queue<Particle*> makeStack(Source s , int size) {
   for(int i=0 ; i < size ; ++i) {
 
     //  sample from emission angle
-    double xi    = Urand();
-    bool   found = false;
-    float  prob = 0;
-    float  mu;
+    double  xi    = Urand();
+    bool    found = false;
+    double  prob  = 0;
+    double  mu;
     
     for(int i = 0; i < s.mu_probs.size(); ++i) {
         if(xi > prob) {
@@ -172,20 +175,75 @@ std::queue<Particle*> makeStack(Source s , int size) {
 //	                   							         	                                                 //
 // ********************************************************************************************************* //
 
-void transport(Tally &leak_tally , Tally &flux_tally, Slab s , std::queue<Particle*> stack) {
+void transport(Leakage &leak_tally , Flux &flux_tally, Slab s , std::queue<Particle*> stack) 
+{
 //  this function takes in pointers to a slab and tally objects, and a pointer to a stack of particles, 
 //  modifies tally objects tally objects 
 //  for each particle in the stack
 //  run transport
-  while(! stack.empty())
-  {  
+  double b = s.width / flux_tally.nbins;
+  while(! stack.empty()){  
     
+    while ( stack.front()->alive ) {
+      // do transport
 
-    Particle * tmp = stack.front();
-    stack.pop(); // get rid of the pointer to the particle 
-    delete(&tmp); // destruct the memory holding the particle
-  }
+      double optical_dist = -log( Urand() ); // optical distance to collison
+      double x1           = stack.front()->x;
 
+      stack.front()->x += optical_dist * stack.front()->mu / s.total_xs;
+      double delta_x = stack.front()->x - x1;
+
+      // check for leakage
+      if ( stack.front()->x < 0.0 || stack.front()->x > s.width ) {
+        // leaked out
+        stack.front()->alive = false;
+        if ( stack.front()->x < 0.0 ) {
+          // update distance traveled and position to start of the slab
+          stack.front()-> x = 0;
+          optical_dist = s.total_xs * x1 / std::abs(stack.front()->mu); 
+        // tally if leaked out of right side
+        }
+        if ( stack.front()->x > s.width ) {
+          //update leakage tally
+          //leakage_hist += 1.0;
+          // update distance traveled and position to end of the slab 
+          stack.front()->x = s.width;
+          optical_dist = s.total_xs * ( stack.front()->x - x1 ) / std::abs(stack.front()->mu);
+        }
+      }
+
+      // update path flux tally in each spatial bin
+      // indexing from 0
+      int    oldBin  = floor(x1 / b);
+      int    newBin  = floor(stack.front()->x / b);          
+      
+      // if the particle stayed in the same bin,
+      // add the whole path length to the sum in that bin
+      if ( oldBin == newBin ) {
+      //   path_lengths[oldBin] += optical_dist / s.total_xs;
+      }
+      //  otherwise iterate through the bins the particle passed through, 
+      //  incrementing them by the path length traversed in that bin
+      else {
+        //sort the bins and positions the particles moved between
+        bool forward = newBin > oldBin; 
+        
+        int    smallBin   = (forward) ? (oldBin):(newBin);
+        int    largeBin   = (forward) ? (newBin):(oldBin);
+        double small_x    = (forward) ? (x1):(stack.front()->x);
+        double large_x    = (forward) ? (stack.front()->x):(x1);
+      }
+      //    path_lengths[smallBin] += dist_to_collision * (( smallBin +1 ) * b - small_x ) / delta_x;
+
+
+      if(stack.front()->alive == false) {
+        // destruct particle object and remove ptr from stack
+        Particle * tmp = stack.front();
+        stack.pop(); // get rid of the pointer to the particle 
+        delete(&tmp); // destruct the memory holding the particle
+      }
+    } // end of current particle
+  } // end of particle stack
 };
 
 // ********************************************************************************************************* //
@@ -198,12 +256,16 @@ int main()  {
   // set up transport problem
   //TODO create input class constructor that parses from input file
   Input I; 
-  I.nHistories       = 10;
+  I.nHistories       = 1;
   I.slab.width       = 4.0;
   I.slab.nCells      = 4;
-  I.slab.total_xs    = {0.0 , 0.3 , 1.0 , 1.0 } ;
-  I.slab.scatter_xs  = {0.0 , 0.25 , 0.7 , 0.1};
-  I.slab.fission_xs  = {0.0, 0.0 , 0.0 , 0.0};
+  I.slab.total_xs    = 1.0;
+  I.slab.scatter_xs  = 0.0;
+  I.slab.fission_xs  = 0.0;
+
+//  I.slab.total_xs    = {0.0 , 0.3  , 1.0 , 1.0} ;
+//  I.slab.scatter_xs  = {0.0 , 0.25 , 0.7 , 0.1};
+//  I.slab.fission_xs  = {0.0 , 0.0  , 0.0 , 0.0};
 
   // define a source
   Source s;
@@ -221,13 +283,10 @@ int main()  {
   Flux    flux_tally;
   flux_tally.nbins = 1000; //the mesh for the flux tally has 1000 bins
 
-  //I.tallies["Leakage Probability"] = leak_tally;
-  //I.tallies["Flux"] = flux_tally;
   
-
   // run and time the transport function
   std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
-  transport(I.tallies["Leakage Probability"], I.tallies["Flux"], I.slab , stack);
+  transport(leak_tally, flux_tally, I.slab , stack);
   std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
 
   double duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
